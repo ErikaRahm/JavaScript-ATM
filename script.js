119038101c11366797a4f6c1efb331edb05db938
Erika Rahm <raffelina84@outlook.com>
2022-12-15 08:52:49 +0100

    new-repo

---
+++ script.js
@@ -0,0 +1,84 @@
+// BANK ACCOUNT
+
+const account = {
+    accountName: "Erika Rahm",
+    balance: 4000,
+    getBalance: function () {
+      alert("The balance is: " + this.balance);
+      atm();
+    },
+    deposit: function () {
+      let dep = parseFloat(prompt("How much would you like to deposit?"));
+      if (isNaN(dep) || dep === "" || dep === null || dep <= 0) {
+        alert(this.accountError());
+        this.deposit();
+      } else {
+        this.balance += dep;
+        this.getBalance();
+      }
+      atm();
+    },
+    withdrawal: function () {
+      let withDraw = parseFloat(prompt("How much would you like to withdraw?"));
+      if (
+        isNaN(withDraw) ||
+        withDraw === "" ||
+        withDraw === null ||
+        withDraw <= 0
+      ) {
+        alert(this.accountError());
+        this.withdrawal();
+      } else if (withDraw > this.balance) {
+        alert(
+          "Amount is greater than current balance: Please try another amount."
+        );
+        this.withdrawal();
+      } else {
+        this.balance -= withDraw;
+        this.getBalance();
+      }
+      atm();
+    },
+    getAccountName: function () {
+      const accName = alert("Name of account: " + this.accountName);
+      console.log(accName);
+      atm();
+    },
+    accountError: function () {
+      alert("Incorrect input: Please enter a number between 1-5");
+      atm();
+    },
+    exitAccount: function () {
+      const exit = confirm("See you next time");
+      console.log(exit);
+      if (exit) {
+        window.close();
+      } else {
+        atm();
+      }
+    },
+  };
+  
+  // ATM
+  function atm() {
+    let choice = parseInt(
+      prompt(
+        "Select a choice 1.) See balance 2.) Make a deposit 3.) Make a withdrawal 4.) Get account name 5.) Exit"
+      )
+    );
+    if (choice === 1) {
+      account.getBalance();
+    } else if (choice === 2) {
+      account.deposit();
+    } else if (choice === 3) {
+      console.log(account.withdrawal());
+    } else if (choice === 4) {
+      console.log(account.getAccountName());
+    } else if (choice === 5) {
+      console.log(account.exitAccount());
+    } else {
+      console.log(account.accountError());
+    }
+  }
+  
+  atm();  
